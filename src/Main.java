b90b9e3b86a8edb9776ff641ba4696eb98222aa6
Ian Henderson <ian.henderson1215@gmail.com>
2023-02-04 23:59:38 -0800

    completed guessing game, with 3 attempts feature and import Random class for the first time

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Scanner userInput = new Scanner(System.in);
+
+        int attemptsLeft = 3;
+        Random rand = new Random();
+        int computerGuess = rand.nextInt(10);
+        computerGuess += 1;
+
+
+        System.out.println("Welcome to the number guessing game!\n");
+
+
+        do {
+            if (attemptsLeft > 0) {
+
+                System.out.println("\nYou have " + attemptsLeft + " attempts left");
+                System.out.println("Please guess a number between 1 - 10");
+                int userGuess = userInput.nextInt();
+
+                if (userGuess == computerGuess) {
+                    System.out.println("Congratulations, you guessed the correct number! The computer chose number " + computerGuess);
+                    break;
+                } else {
+                    System.out.println("Sorry, that was incorrect.");
+
+                    attemptsLeft--;
+                    if (attemptsLeft == 0) {
+                        System.out.println("\n**** Game Over ****");
+                        System.out.println("*The Number was " + computerGuess + " *");
+                        System.out.println("****  You Lose ****");
+                    } else {
+                        System.out.println("Guess Again!");
+                    }
+                }
+            }
+        } while (attemptsLeft > 0) ;
     }
 }
